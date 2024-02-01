64f4d6c1f741ac2719aca79233f092acd6ff9869
uthsasaha <uthsasaha47@gmail.com>
2024-02-01 22:53:54 +0600

    grid section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+        }
+      },
     },
     plugins: [],
   }
